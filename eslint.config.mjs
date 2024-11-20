cc39319b033715f1bf6222610cfdea3507ecbeb8
Cristina Poncela Cubeiro <140309543+cristinaponcela@users.noreply.github.com>
2024-11-20 13:59:53 +0100

    ESLint!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,9 @@
-import eslintPluginReact from 'eslint-plugin-react';
-import typescriptEslintParser from '@typescript-eslint/parser';
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+// @ts-check
 
-export default {
-  ignores: ['node_modules/'], // Files and folders to ignore
-  files: ['**/*.ts', '**/*.tsx'], // Specify the files to lint
-  languageOptions: {
-    ecmaVersion: 2020, // Support for modern JavaScript
-    sourceType: 'module', // Enable ES module syntax
-    parser: typescriptEslintParser, // Use the TypeScript parser
-  },
-  plugins: {
-    '@typescript-eslint': typescriptEslintPlugin,
-    react: eslintPluginReact,
-  },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect React version
-    },
-  },
-  rules: {
-    'no-console': 'warn', // Warn for console statements
-    '@typescript-eslint/no-unused-vars': ['error'], // Error for unused variables
-  },
-};
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
