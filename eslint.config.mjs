9b1a9a30cbda0fbe86867e56558211b4de316d0d
Cristina Poncela Cubeiro <140309543+cristinaponcela@users.noreply.github.com>
2024-11-20 12:59:24 +0100

    Lint Config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-
-/** @type {import('eslint').Linter.Config[]} */
+// eslint.config.js
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+      files: ["src/**/*.{js, ts, mjs, json}"],
+      ignores: ["**/*.config.js", "!**/eslint.config.js"],
+      rules: {
+          semi: "error"
+      }
+  }
 ];
